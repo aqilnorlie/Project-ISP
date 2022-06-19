24f3a6c10494129cd6761b078b9c99e17f071c45
HanifPaan <hanifmuhamad2811@gmail.com>
2022-06-19 23:42:46 +0800

    Terms, Icons

---
+++ MyraTerms/peditterms.php
@@ -0,0 +1,141 @@
+<?php
+
+include('../MyraSubSection/sconnection.php');
+session_start();
+
+//database connect
+// $host = 'localhost';
+// $dbname1 = 'myra';
+// $username = 'root';
+// $password = '';
+
+// $dsn = "mysql:host=$host;dbname=$dbname1;";
+
+// $options = [
+//     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//     PDO::ATTR_EMULATE_PREPARES   => false,
+// ];
+
+// try {
+//     $pdo = new PDO($dsn, $username, $password, $options);
+// } catch (\PDOException $e) {
+//     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+// }
+
+if(isset($_POST['submit_update']))
+{
+    // $sectionid = $_POST['sectionId'];
+    // $sectionNumber = $_SESSION["sectionNumberNew"];
+    $sectionNumberTerm = $_SESSION["sectionNumberTerm"];
+    $subSectionTitleMalayTerm = $_SESSION["subSectionTitleMalayTerm"];
+    $termToken = $_SESSION["termToken"];
+
+    // $_POST['sectionNumber'];
+    $termTitleMalay = $_POST['termTitleMalay'];
+    $termTitleEnglish = $_POST['termTitleEnglish'];
+    $termDescription = $_POST['termDescription'];
+    
+    try{
+        if($_POST['dataStatus'] == 0)
+        {
+            $query = "UPDATE myraterm
+            SET 
+                termTitleMalay=:termTitleMalay, 
+                termTitleEnglish=:termTitleEnglish, 
+                termDescription=:termDescription,
+                updatedAt=now(),
+                dataStatusId=:dataStatusId
+            WHERE 
+                token=:token LIMIT 1";
+
+            $statement = $pdo->prepare($query);
+
+            $data = [
+                ':termTitleMalay' => $termTitleMalay,
+                ':termTitleEnglish' => $termTitleEnglish,
+                ':termDescription' => $termDescription,
+                // ':sectionId' => $sectionid,
+                ':token' => $termToken,
+                ':dataStatusId' => 0
+            ];
+
+            $query_execute = $statement->execute($data);
+
+            if($query_execute)
+            {
+                // $_SESSION['message'] = "Section HAS been updated.";
+                header('Location: Terms.php');
+                exit(0);
+                // echo 'Section HAS been updated.';
+            }
+            else
+            {
+                // $_SESSION['message'] = "Section has NOT been updated.";
+                header('Location: Terms.php');
+                exit(0);
+                // echo 'Section has NOT been updated.';
+            }
+        }
+
+        else if($_POST['dataStatus'] == 1)
+        {
+            $query = "UPDATE myraterm
+            SET 
+                termTitleMalay=:termTitleMalay, 
+                termTitleEnglish=:termTitleEnglish, 
+                termDescription=:termDescription,
+                updatedAt=now(),
+                dataStatusId=:dataStatusId
+            WHERE 
+                token=:token LIMIT 1";
+
+            $statement = $pdo->prepare($query);
+
+            $data = [
+                ':termTitleMalay' => $termTitleMalay,
+                ':termTitleEnglish' => $termTitleEnglish,
+                ':termDescription' => $termDescription,
+                // ':sectionId' => $sectionid,
+                ':token' => $termToken,
+                ':dataStatusId' => 1
+            ];
+
+            $query_execute = $statement->execute($data);
+
+            if($query_execute)
+            {
+                // $_SESSION['message'] = "Section HAS been updated.";
+                header('Location: Terms.php');
+                exit(0);
+                // echo 'Section HAS been updated.';
+            }
+            else
+            {
+                // $_SESSION['message'] = "Section has NOT been updated.";
+                header('Location: Terms.php');
+                exit(0);
+                // echo 'Section has NOT been updated.';
+            }
+        }
+        
+    }
+
+    catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+    // $query = "UPDATE myrasection
+    // SET sectionTitleMalay='$titlemy', 
+    // sectionTitleEnglish='$titleeng', 
+    // sectionDescription='$desc' 
+    // WHERE sectionNumber='$sectionnumber'";
+
+    // // $query_run = mysqli_query($con, $query);
+
+    // $stmt = $pdo->query($query);
+
+    // echo 'Section has been updated.';
+
+?>
