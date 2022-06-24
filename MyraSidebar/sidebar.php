88c0f21f68536e73bdded942e23ea8a79fd2134f
aqil <datapirates69@gmail.com>
2022-06-24 23:47:47 +0800

    update on login,search,sidebar

--- MyraSidebar/sidebar.php
+++ MyraSidebar/sidebar.php
@@ -43,7 +43,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="../dist/img/lecturerjpg.jpg" class="img-circle elevation-2" alt="User Image" style="width:50px; vertical-align:center;">
         </div>
         <div class="info">
           
